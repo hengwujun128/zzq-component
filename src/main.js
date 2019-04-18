8f25f066e7927720e9e3bef0a88d90204ed07bed
天山来客 <hengwujun128@gmail.com>
2019-04-18 23:33:06 +0800

    test vue-cli3 plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import zzq from "./components";
+import VueRx from 'vue-rx';
+import VueRx from 'vue-rx';
+
+Vue.use(VueRx);
+
+Vue.use(VueRx);
 Vue.config.productionTip = false;
 
 Vue.use(zzq);
