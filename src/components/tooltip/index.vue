18998ddac70bbf04690c6e873f45b344bb2ae309
天山来客 <hengwujun128@gmail.com>
2019-05-04 01:22:09 +0800

    fix: repire bugs of tooltip

--- src/components/tooltip/index.vue
+++ src/components/tooltip/index.vue
@@ -1,94 +1,125 @@
 <template>
-  <div>
-    <h2>hello,this is tooltip component</h2>
-    <!-- <div class="tooltip">Hover over me
-      <span class="tooltiptext">Tooltip text</span>
-    </div> -->
-    <tooltip position="left">
-      <div>
-        <div class="title">hello,title</div>
-        <div class="desc">hello,desc</div>
-      </div>
-    </tooltip>
+  <div class="tooltip-container">
+    <div class="tooltip">
+      HoverMe
+      <span :class="[common, pos]">
+        <slot></slot>
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-import tooltip from '../../../components/tooltip';
 export default {
+  props: {
+    position: {
+      type: String,
+      default: 'top'
+    },
+  },
   data () {
     return {
-
+      common: 'common'
     }
   },
-  components: {
-    tooltip
+  computed: {
+    pos () {
+      return `position-${this.position}`
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-/* Tooltip container */
-.tooltip {
-  position: relative;
-  display: inline-block;
-  border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
-  cursor: pointer;
-}
+.tooltip-container {
+  .tooltip {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
 
-/* Tooltip text */
-.tooltip .tooltiptext {
-  visibility: hidden;
-  width: 120px;
-  background-color: #555;
-  color: #fff;
-  text-align: center;
-  padding: 5px 0;
-  border-radius: 6px;
+    .common {
+      visibility: hidden;
+      width: 120px;
+      background-color: #555;
+      color: #fff;
+      text-align: center;
+      padding: 5px 0;
+      border-radius: 6px;
+      /* Position the tooltip text */
+      position: absolute;
+      z-index: 1;
+      /* Fade in tooltip */
+      opacity: 0;
+      transition: opacity 1s;
+    }
+    /* left */
+    .position-left {
+      right: 110%;
+      top: 50%;
+      transform: translateY(-50%);
+      &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        border-width: 5px;
+        transform: translateY(-50%);
+        border-style: solid;
+        border-color: transparent transparent transparent #555;
+      }
+    }
+    .position-top {
+      bottom: 120%;
+      left: 50%;
+      transform: translateX(-50%);
+      &::after {
+        content: '';
+        position: absolute;
+        left: 50%;
+        top: 100%;
+        border-width: 5px;
+        transform: translateX(-50%);
+        border-style: solid;
+        border-color: #555 transparent transparent transparent;
+      }
+    }
+    .position-right {
+      left: 120%;
+      top: 50%;
+      transform: translateY(-50%);
+      &::after {
+        content: '';
+        position: absolute;
+        right: 100%;
+        top: 50%;
+        transform: translateY(-50%);
+        border-width: 5px;
+        border-style: solid;
+        border-color: transparent #555 transparent transparent;
+      }
+    }
+    .position-bottom {
+      top: 120%;
+      left: 50%;
+      transform: translateX(-50%);
+      &::after {
+        content: '';
+        position: absolute;
+        left: 50%;
+        bottom: 100%;
+        transform: translateX(-50%);
+        border-width: 5px;
+        border-style: solid;
+        border-color: transparent transparent #555 transparent;
+      }
+    }
 
-  /* Position the tooltip text */
-  position: absolute;
-  z-index: 1;
-  // top
-  // bottom: 125%;
-  // left: 50%;
-  // margin-left: -60px;
-
-  /* left */
-  right: 110%;
-  top: 50%;
-  transform: translateY(-50%);
-
-  /* Fade in tooltip */
-  opacity: 0;
-  transition: opacity 1s;
-
-  &::after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 100%;
-    border-width: 5px;
-    transform: translateY(-50%);
-    border-style: solid;
-    border-color: transparent transparent transparent #555;
+    &:hover {
+      .common {
+        visibility: visible;
+        opacity: 1;
+      }
+    }
   }
-}
-
-/* Tooltip arrow */
-// .tooltip .tooltiptext::after {
-//   content: '';
-//   position: absolute;
-//   top: 100%;
-//   left: 50%;
-//   margin-left: -5px;
-//   border-width: 5px;
-//   border-style: solid;
-//   border-color: #555 transparent transparent transparent;
-// }
-/* Show the tooltip text when you mouse over the tooltip container */
-.tooltip:hover .tooltiptext {
-  visibility: visible;
-  opacity: 1;
 }
 </style>
